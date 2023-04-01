dc22604c9ce05378e549b764f5f26cf0b7a69b86
devplay-mrp <devplay.mrp@gmail.com>
2023-04-01 06:52:04 -0300

    Componente Footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-
+import VideoFooter from "./components/VideoFooter";
 import "./video.css";
 
 
@@ -31,6 +31,8 @@ function Video() {
                 loop
                 src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
             ></video>
+            {/*  side bar */}
+            <VideoFooter />
             
 
         </div>
