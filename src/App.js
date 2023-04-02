0a5f26b9aaedc8b1836a7aca61aa01a6dfa4602d
devplay-mrp <devplay.mrp@gmail.com>
2023-04-02 11:26:31 -0300

    Componente Sidebar e Preparação do UI para receber DB

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ function App() {
   return (
     <div className="App">
       <div className= 'app__videos'> 
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes = {111}
+          messages = {222}
+          shares = {333} 
+          name= "Paulo"
+          description= "Brecker o goleiro"
+          music= "música épica" 
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4 "    
+        />
+
+        <Video 
+          likes = {444}
+          messages = {555}
+          shares = {666}   
+          name= "Pedro"
+          description= "gato cantando"
+          music= "música qualquer" 
+          url= "https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"    
+        />
+
       </div>
 
     </div>
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from "react";
-import VideoFooter from "./components/VideoFooter";
+import VideoFooter from "./components/footer/VideoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 import "./video.css";
 
 
 
-function Video() {
+function Video({likes, messages, shares, name, description, music, url}) {
 
     const videoRef = useRef(null);
     const [play, setPlay] = useState(false);
@@ -29,10 +30,21 @@ function Video() {
                 ref={videoRef}
                 onClick={handdleStart}
                 loop
-                src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+                src= {url}
             ></video>
-            {/*  side bar */}
-            <VideoFooter />
+
+            <VideoSidebar 
+                likes= {likes}
+                messages= {messages}
+                shares={shares}
+            
+            />
+            <VideoFooter 
+            
+            name= {name}
+            description= {description}
+            music= {music}
+            />
             
 
         </div>
